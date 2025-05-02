2f32de73042c62159dca50d3512dbe6910e03255
TimChel <timchelyadinsky@yandex.by>
2025-05-02 15:49:39 +0300

    создает def create_user REST-эндпоинт
    Создает эндпоинт для добавления нового пользователя вручную

--- app/routes/user.py
+++ app/routes/user.py
@@ -6,12 +6,23 @@ from app.data_handler import (add_user_to_db)
 from app.db import get_session
 from sqlmodel import Session
 from ..auth import auth_handler
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import UniqueViolation
 
 router = APIRouter(prefix="/user", tags=("Работа с данными пользователей"))
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=model.UserCreate, summary="Создать нового пользователя вручную")
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=model.UserGet_by_Admin, summary="Создать нового пользователя вручную")
 def create_user(user: Annotated[model.UserCreate, request_examples.exampl_create_user], session: Session = Depends(get_session)):
-    extra_data = auth_handler.
+    extra_data = {"hash_password": auth_handler.get_password_hash(user.password)}
     new_user = model.User.model_validate(user, update=extra_data)
-    new_user = add_user_to_db(user)
+    try:
+        new_user = add_user_to_db(new_user)
+        # session.add(new_user)
+        # session.commit()
+        # session.refresh(new_user)
+        return new_user
+    except IntegrityError as e:
+        assert isinstance(e.orig, UniqueViolation)
+        raise HTTPExeption(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=f"Пользоатель с email {user.email} уже существует")
+    
