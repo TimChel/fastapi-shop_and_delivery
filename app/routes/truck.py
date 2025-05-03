0fb657c06cd829e9a5e37c77c9b27ddab89faf05
TimChel <timchelyadinsky@yandex.by>
2025-05-03 18:58:12 +0300

    Создает эндпоинт для удаления грузовика

--- app/routes/truck.py
+++ app/routes/truck.py
@@ -2,7 +2,7 @@ from typing import Annotated
 from fastapi import APIRouter, status, Depends, HTTPException
 from app.models import model
 from app.api_docs import request_examples
-from app.data_handler import (add_truck_to_db)
+from app.data_handler import (add_truck_to_db, delete_data)
 from app.db import get_session
 from sqlmodel import Session, select
 from app.auth import auth_handler
@@ -36,25 +36,14 @@ def create_truck(truck: model.TruckCreate, current_user: Annotated[model.User, D
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Для использования данной функции необходимо иметь уровень доступа 'admin'")
     new_truck = add_truck_to_db(truck, session)
     return new_truck
-#
-# @router.patch("/update/{id_product}", status_code=status.HTTP_200_OK, response_model=model.ProductWithProviderGet)
-# def update_user(id_product: int, product: model.ProductUpdate, current_user: Annotated[model.User, Depends(auth_handler.get_current_user)], session: Session = Depends(get_session)):
-#     db_product = session.get(model.Product, id_product)
-#     if not db_product:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Товара с id {id_product} не существет")
-#     if current_user.access_level.name != "admin" and db_product.provider.user_id != current_user.user_id:
-#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-#                             detail=f"Изменение товара другого пользователя запрещено ")
-#     updated_product = update_product_p(db_product, product, session)
-#     return updated_product
-#
-# @router.delete("/delete/{id_product}", status_code=status.HTTP_200_OK)
-# def delete_product_by_id(id_product: int, current_user: Annotated[model.User, Depends(auth_handler.get_current_user)], session: Session = Depends(get_session)):
-#     db_product = session.get(model.Product, id_product)
-#     if not db_product:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Товара с id {id_product} не существет")
-#     if current_user.access_level.name != "admin" and db_product.provider.user_id != current_user.user_id:
-#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-#                             detail=f"Удаление товара другого пользователя запрещено")
-#     db_product = delete_data(db_product, session)
-#     return f"Товар {db_product.name} удален"
+
+@router.delete("/delete/{truck_id}", status_code=status.HTTP_200_OK)
+def delete_truck_by_id(truck_id: int, current_user: Annotated[model.User, Depends(auth_handler.get_current_user)], session: Session = Depends(get_session)):
+    db_truck = session.get(model.Truck, truck_id)
+    if not db_truck:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Грузовика с id {truck_id} не существет")
+    if current_user.access_level.name != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail=f"Удаление грузовика запрещено")
+    db_truck = delete_data(db_truck, session)
+    return f"Грузовик удален"
